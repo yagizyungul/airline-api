8b144b0c26fdd92b8cc8182fac8bb444c7219cfd
Yagiz Yungul <yagizyy12@gmail.com>
2026-04-19 00:10:54 +0300

    Fix FlightDTO to return full flight data, increase rate limit to 50/day

--- src/dto/flightDTO.js
+++ src/dto/flightDTO.js
@@ -1,7 +1,13 @@
 class FlightDTO {
   constructor(flight) {
     this.flightNumber = flight.flight_number;
+    this.dateFrom = flight.date_from;
+    this.dateTo = flight.date_to;
+    this.airportFrom = flight.airport_from;
+    this.airportTo = flight.airport_to;
     this.duration = flight.duration;
+    this.capacity = flight.capacity;
+    this.remainingSeats = flight.remaining_seats;
   }
 }
 
